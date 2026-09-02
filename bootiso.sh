0de3aec73a4acfcd49d89e2d1427514c73eb12ed
jredfox <jredfox_@outlook.com>
2026-09-01 22:17:17 -0500

    WIP

--- bootiso.sh
+++ bootiso.sh
@@ -15,25 +15,28 @@ if [ -z "$LWDE" ]; then
 fi
 
 if [ "$arch" = "aarch64" ]; then
+  mkdir -p "firmware"
+  cp "/usr/share/AAVMF/AAVMF_CODE.fd" "firmware/AAVMF_CODE.fd"
+  cp "/usr/share/AAVMF/AAVMF_VARS.fd" "firmware/AAVMF_VARS.fd"
   qemu-system-aarch64 \
-    -cpu cortex-a72 \
-    -machine virt,gic-version=2 \
+    -cpu "cortex-a72" \
+    -machine "virt,gic-version=2" \
     -m "$qram" \
     -smp "$qcore" \
-    -device qemu-xhci \
-    -device usb-kbd \
-    -device usb-tablet \
-    -device virtio-keyboard-pci \
-    -device virtio-mouse-pci \
-    -drive if=pflash,format=raw,unit=0,file=AAVMF_CODE.fd,readonly=on \
-    -drive if=pflash,format=raw,unit=1,file=AAVMF_VARS.fd \
-    -netdev user,id=net0 \
-    -device virtio-net-device,netdev=net0 \
-    -device virtio-rng-pci \
-    -drive file=${cow},format=qcow2,if=virtio,id=VIRTIO1 \
-    -device virtio-scsi-pci,id=scsi0 \
-    -drive file=${iso},format=raw,readonly=on,if=none,id=cdrom0,media=cdrom \
-    -device scsi-cd,drive=cdrom0,bus=scsi0.0 \
+    -device "qemu-xhci" \
+    -device "usb-kbd" \
+    -device "usb-tablet" \
+    -device "virtio-keyboard-pci" \
+    -device "virtio-mouse-pci" \
+    -drive "if=pflash,format=raw,unit=0,file=firmware/AAVMF_CODE.fd,readonly=on" \
+    -drive "if=pflash,format=raw,unit=1,file=firmware/AAVMF_VARS.fd" \
+    -netdev "user,id=net0" \
+    -device "virtio-net-device,netdev=net0" \
+    -device "virtio-rng-pci" \
+    -drive "file=${cow},format=qcow2,if=virtio,id=VIRTIO1" \
+    -device "virtio-scsi-pci,id=scsi0" \
+    -drive "file=${iso},format=raw,readonly=on,if=none,id=cdrom0,media=cdrom" \
+    -device "scsi-cd,drive=cdrom0,bus=scsi0.0" \
     -nographic
   exit $?
 fi
@@ -41,17 +44,17 @@ fi
 #BOOT RISC-V ISO
 if [ "$arch" = "riscv64" ]; then
   qemu-system-riscv64 \
-    -cpu rv64 \
-    -machine virt,acpi=off \
+    -cpu "rv64" \
+    -machine "virt,acpi=off" \
     -m "$qram" \
     -smp "$qcore" \
-    -nographic \
-    -kernel /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf \
-    -netdev user,id=net0 \
-    -device virtio-net-device,netdev=net0 \
-    -device virtio-rng-pci \
-    -drive file=${iso},format=raw,readonly=on,if=virtio \
-    -drive file="${cow}",format=qcow2,if=virtio \
+    -kernel "/usr/lib/u-boot/qemu-riscv64_smode/uboot.elf" \
+    -netdev "user,id=net0" \
+    -device "virtio-net-device,netdev=net0" \
+    -device "virtio-rng-pci" \
+    -drive "file=${iso},format=raw,readonly=on,if=virtio" \
+    -drive "file=${cow},format=qcow2,if=virtio" \
+    -nographic
     exit $?
 fi
 
@@ -66,7 +69,7 @@ if [ "$LWDE" = "true" ]; then
     -cdrom "$iso" \
     -boot d \
     -enable-kvm \
-    -device qxl-vga,vram_size=134217728 \
+    -device "qxl-vga,vram_size=134217728" \
   exit "$?"
 fi
 
--- bootnormal.sh
+++ bootnormal.sh
@@ -16,22 +16,25 @@ sharedir="../share"
 cd "disks"
 
 if [ "$arch" = "aarch64" ]; then
+  mkdir -p "firmware"
+  cp "/usr/share/AAVMF/AAVMF_CODE.fd" "firmware/AAVMF_CODE.fd"
+  cp "/usr/share/AAVMF/AAVMF_VARS.fd" "firmware/AAVMF_VARS.fd"
   qemu-system-aarch64 \
-    -cpu cortex-a72 \
-    -machine virt,gic-version=2 \
+    -cpu "cortex-a72" \
+    -machine "virt,gic-version=2" \
     -m "$qram" \
     -smp "$qcore" \
-    -device qemu-xhci \
-    -device usb-kbd \
-    -device usb-tablet \
-    -device virtio-keyboard-pci \
-    -device virtio-mouse-pci \
-    -drive if=pflash,format=raw,unit=0,file=AAVMF_CODE.fd,readonly=on \
-    -drive if=pflash,format=raw,unit=1,file=AAVMF_VARS.fd \
-    -netdev user,id=net0 \
-    -device virtio-net-device,netdev=net0 \
-    -device virtio-rng-pci \
-    -drive file=${cow},format=qcow2,if=virtio \
+    -device "qemu-xhci" \
+    -device "usb-kbd" \
+    -device "usb-tablet" \
+    -device "virtio-keyboard-pci" \
+    -device "virtio-mouse-pci" \
+    -drive "if=pflash,format=raw,unit=0,file=AAVMF_CODE.fd,readonly=on" \
+    -drive "if=pflash,format=raw,unit=1,file=AAVMF_VARS.fd" \
+    -netdev "user,id=net0" \
+    -device "virtio-net-device,netdev=net0" \
+    -device "virtio-rng-pci" \
+    -drive "file=${cow},format=qcow2,if=virtio" \
     -nographic
   exit $?
 fi
@@ -40,15 +43,15 @@ fi
 if [ "$arch" = "riscv64" ]; then
   qemu-system-riscv64 \
     -cpu rv64 \
-    -machine virt,acpi=off \
+    -machine "virt,acpi=off" \
     -m "$qram" \
     -smp "$qcore" \
     -nographic \
-    -kernel /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf \
-    -netdev user,id=net0 \
-    -device virtio-net-device,netdev=net0 \
-    -device virtio-rng-pci \
-    -drive file="$cow",format=qcow2,if=virtio
+    -kernel "/usr/lib/u-boot/qemu-riscv64_smode/uboot.elf" \
+    -netdev "user,id=net0" \
+    -device "virtio-net-device,netdev=net0" \
+    -device "virtio-rng-pci" \
+    -drive "file=$cow,format=qcow2,if=virtio"
   exit $?
 fi
 
@@ -60,9 +63,9 @@ if [ "$LWDE" = "true" ]; then
     -smp "$qcore" \
     -hda "$cow" \
     -enable-kvm \
-    -device AC97 \
-    -usb -device usb-mouse \
-    -device qxl-vga,vram_size=134217728 \
+    -device "AC97" \
+    -usb -device "usb-mouse" \
+    -device "qxl-vga,vram_size=134217728" \
     -virtfs "local,path=$sharedir,mount_tag=hostshare,security_model=none"
   exit "$?"
 fi
