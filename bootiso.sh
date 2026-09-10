322f493fb18a2a7cdc9f0fd9bae91110bab9fb39
jredfox <jredfox_@outlook.com>
2026-09-10 01:29:00 -0500

    Update bootiso.sh

--- bootiso.sh
+++ bootiso.sh
@@ -245,12 +245,12 @@ if [ "$family_target" = "arm" ]; then
       AAVMF_CODE_PATH="/usr/share/AAVMF/AAVMF_CODE.fd"
       AAVMF_VARS_PATH="/usr/share/AAVMF/AAVMF_VARS.fd"
       AAVMF_CODE="$fwrdir/AAVMF_CODE.fd"
-      AAVMF_VARS="$fwrdir/${dname}_iso.fd"
+      AAVMF_VARS="$fwrdir/${dname}_AAVMF_VARS_iso.fd"
     else
       AAVMF_CODE_PATH="/usr/share/AAVMF/AAVMF32_CODE.fd"
       AAVMF_VARS_PATH="/usr/share/AAVMF/AAVMF32_VARS.fd"
       AAVMF_CODE="$fwrdir/AAVMF_CODE32.fd"
-      AAVMF_VARS="$fwrdir/${dname}32_iso.fd"
+      AAVMF_VARS="$fwrdir/${dname}_AAVMF32_VARS_iso.fd"
     fi
     #Optimization
     if [ ! -f "$AAVMF_CODE" ]; then
