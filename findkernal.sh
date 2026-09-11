d67718c08adc8ae390d66d9c7d6803722ea6aaf1
jredfox <jredfox_@outlook.com>
2026-09-11 17:42:15 -0500

    improve kernal finder

--- findkernal.sh
+++ findkernal.sh
@@ -3,10 +3,10 @@ if [ -z "$iso" ]; then
 	read -p "Enter Linux ISO:" iso
 	iso="$(printf '%s' "$iso" | sed 's/^["'\'']//; s/["'\'']$//')"
 fi
-results="$(7z l -ba "${iso}" | awk 'substr($3,1,1) != "D" { sub(/^([^ ]+ +){5}/, "") ; print }' | sed 's|^[^/]|/&|' | grep -Ei '^(/[^/]+){0,4}/(hwe-)?(vmlinuz|zImage|uImage|bzImage|Image|linux|vmlinux|initrd|uInitrd|initramfs|initramfs-linux)(-virt)?(-lts)?(-virt)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$')"
+results="$(7z l -ba "${iso}" | awk 'substr($3,1,1) != "D" { sub(/^([^ ]+ +){5}/, "") ; print }' | sed 's|^[^/]|/&|' | grep -Ei '^(/[^/]+){0,4}/(hwe-)?(vmlinuz|zImage|uImage|bzImage|Image|linux|vmlinux|initrd|uInitrd|initramfs|initramfs-linux)(-rt|-cloud|-virt|-vm)?(-lts)?(-rt|-cloud|-virt|-vm)?(-lts)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$')"
 results_sorted="$(printf '%s' "$results" | awk -F/ '{ print NF-1, $0 }' | sort -n -k1,1 -k2,2 | sed 's|^[^/]*/||')"
-vmlinuz_path="$(printf '%s' "$results_sorted" | grep -Ei '(hwe-)?(vmlinuz|zImage|uImage|bzImage|Image|linux|vmlinux)(-virt)?(-lts)?(-virt)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$')"
-initrd_path="$(printf '%s' "$results_sorted" | grep -Ei '(hwe-)?(initrd|uInitrd|initramfs|initramfs-linux)(-virt)?(-lts)?(-virt)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$')"
+vmlinuz_path="$(printf '%s' "$results_sorted" | grep -Ei '(hwe-)?(vmlinuz|zImage|uImage|bzImage|Image|linux|vmlinux)(-rt|-cloud|-virt|-vm)?(-lts)?(-rt|-cloud|-virt|-vm)?(-lts)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$')"
+initrd_path="$(printf '%s' "$results_sorted" | grep -Ei '(hwe-)?(initrd|uInitrd|initramfs|initramfs-linux)(-rt|-cloud|-virt|-vm)?(-lts)?(-rt|-cloud|-virt|-vm)?(-lts)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$')"
 echo "kernals:"
 echo "$vmlinuz_path"
 echo "initrd:"
