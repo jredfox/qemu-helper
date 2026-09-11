3431e24931acaf97d9997f7017b47bdc882b5723
jredfox <jredfox_@outlook.com>
2026-09-11 18:32:11 -0500

    allow all keyword combos

--- bootiso.sh
+++ bootiso.sh
@@ -140,7 +140,7 @@ if [ "$kb" = "true" ]; then
   vmlinuz_path="$kb_path"
   initrd_path="$kb_initrd"
   if [ -z "$vmlinuz_path" ] || [ -z "$initrd_path" ]; then
-    results="$(7z l -ba "${iso}" | awk 'substr($3,1,1) != "D" { sub(/^([^ ]+ +){5}/, "") ; print }' | sed 's|^[^/]|/&|' | grep -Ei '^(/[^/]+){0,4}/(hwe-)?(vmlinuz|zImage|uImage|bzImage|Image|linux|vmlinux|initrd|uInitrd|initramfs|initramfs-linux)(-rt|-cloud|-virt|-vm|-generic|-hwe)?(-lts)?(-rt|-cloud|-virt|-vm|-generic|-hwe)?(-lts)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$')"
+    results="$(7z l -ba "${iso}" | awk 'substr($3,1,1) != "D" { sub(/^([^ ]+ +){5}/, "") ; print }' | sed 's|^[^/]|/&|' | grep -Ei '^(/[^/]+){0,4}/(hwe-)?(vmlinuz|zImage|uImage|bzImage|Image|linux|vmlinux|initrd|uInitrd|initramfs|initramfs-linux)(-rt|-cloud|-virt|-vm|-generic|-lts|-hwe){0,7}(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$')"
     results_sorted="$(printf '%s' "$results" | awk -F/ '{ print NF-1, $0 }' | sort -n -k1,1 -k2,2 | sed 's|^[^/]*/||')"
     #Prefer vmlinuz/initrd one directory deep for install and boot dirs
     installboot="$(printf '%s' "$results_sorted" | grep -Ei '^(install|boot|efi)[^/]*/[^/]+$')"
@@ -152,16 +152,16 @@ if [ "$kb" = "true" ]; then
         results_sorted="$netboot"
       fi
     fi
-    vmlinuz_path="$(printf '%s' "$results_sorted" | grep -Ei '(hwe-)?(vmlinuz|zImage|uImage|bzImage|Image|linux|vmlinux)(-rt|-cloud|-virt|-vm|-generic|-hwe)?(-lts)?(-rt|-cloud|-virt|-vm|-generic|-hwe)?(-lts)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$' | head -n 1)"
-    initrd_path="$(printf '%s' "$results_sorted" | grep -Ei '(hwe-)?(initrd|uInitrd|initramfs|initramfs-linux)(-rt|-cloud|-virt|-vm|-generic|-hwe)?(-lts)?(-rt|-cloud|-virt|-vm|-generic|-hwe)?(-lts)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$' | head -n 1)"
+    vmlinuz_path="$(printf '%s' "$results_sorted" | grep -Ei '(hwe-)?(vmlinuz|zImage|uImage|bzImage|Image|linux|vmlinux)(-rt|-cloud|-virt|-vm|-generic|-lts|-hwe){0,7}(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$' | head -n 1)"
+    initrd_path="$(printf '%s' "$results_sorted" | grep -Ei '(hwe-)?(initrd|uInitrd|initramfs|initramfs-linux)(-rt|-cloud|-virt|-vm|-generic|-lts|-hwe){0,7}(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$' | head -n 1)"
   else
     echo "skipping dynamic kernal fetch"
   fi
   7z e "${iso}" "$vmlinuz_path" "$initrd_path" -mtc -mta -mtm -aou -y >/dev/null
   echo "kernal: $vmlinuz_path initrd: $initrd_path"
   cd "$opwd"
-  kbkernal="$(find "$kbdir" -maxdepth 1 -type f | grep -Ei '/(hwe-)?(vmlinuz|zImage|uImage|bzImage|Image|linux|vmlinux)(-rt|-cloud|-virt|-vm|-generic|-hwe)?(-lts)?(-rt|-cloud|-virt|-vm|-generic|-hwe)?(-lts)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$' | head -n 1)"
-  kbinitrd="$(find "$kbdir" -maxdepth 1 -type f | grep -Ei '/(hwe-)?(initrd|uInitrd|initramfs|initramfs-linux)(-rt|-cloud|-virt|-vm|-generic|-hwe)?(-lts)?(-rt|-cloud|-virt|-vm|-generic|-hwe)?(-lts)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$' | head -n 1)"
+  kbkernal="$(find "$kbdir" -maxdepth 1 -type f | grep -Ei '/(hwe-)?(vmlinuz|zImage|uImage|bzImage|Image|linux|vmlinux)(-rt|-cloud|-virt|-vm|-generic|-lts|-hwe){0,7}(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$' | head -n 1)"
+  kbinitrd="$(find "$kbdir" -maxdepth 1 -type f | grep -Ei '/(hwe-)?(initrd|uInitrd|initramfs|initramfs-linux)(-rt|-cloud|-virt|-vm|-generic|-lts|-hwe){0,7}(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$' | head -n 1)"
   
   #Set the qemu console serial type needed for kernal booting
   qconsole="$kb_console"
