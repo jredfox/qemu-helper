c84dd515a475af61b3bf9706ebff7ccb6e27e9d4
jredfox <jredfox_@outlook.com>
2026-09-11 02:05:24 -0500

    fix sort algorithm

--- findkernal.sh
+++ findkernal.sh
@@ -4,7 +4,7 @@ if [ -z "$iso" ]; then
 	iso="$(printf '%s' "$iso" | sed 's/^["'\'']//; s/["'\'']$//')"
 fi
 results="$(7z l -ba "${iso}" | awk 'substr($3,1,1) != "D" { sub(/^([^ ]+ +){5}/, "") ; print }' | sed 's|^[^/]|/&|' | grep -Ei '^(/[^/]+){0,4}/(hwe-)?(vmlinuz|zImage|uImage|bzImage|Image|linux|vmlinux|initrd|uInitrd|initramfs|initramfs-linux|vmlinuz-virt)(-lts)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$' | sed 's|^/||')"
-results_sorted="$(printf '%s' "$results" | awk '{print length, $0}' | sort -n | cut -d' ' -f2-)"
+results_sorted="$(printf '%s\n' "$results" | awk -F/ '{ print NF-1, $0 }' | sort -n -k1,1 -k2,2 | sed 's|^[^/]*/|/|')"
 vmlinuz_path="$(printf '%s' "$results_sorted" | grep -Ei '(hwe-)?(vmlinuz|zImage|uImage|bzImage|Image|linux|vmlinux|vmlinuz-virt)(-lts)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$')"
 initrd_path="$(printf '%s' "$results_sorted" | grep -Ei '(hwe-)?(initrd|uInitrd|initramfs|initramfs-linux)(-lts)?(\.gz|\.lz|\.img|\.tar\.gz|\.cpio\.gz)?$')"
 echo "kernals:"
