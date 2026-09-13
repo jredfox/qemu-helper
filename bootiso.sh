f1151f1cf4bd2fe83c65e5430a5042f545676a79
jredfox <jredfox_@outlook.com>
2026-09-13 03:21:42 -0500

    Update bootiso.sh

--- bootiso.sh
+++ bootiso.sh
@@ -389,13 +389,7 @@ case "$arch" in
     qdrive "-cdrom \"$iso\""
     qdrive "-hda \"$cow\""
     qdrive "-boot d"
-    if [ "$kb" == "true" ]; then
-      if [ -z "$kb_console" ]; then
-        if [ "$qconsole" = "ttyS0" ]; then
-          qconsole="tty0"
-        fi
-      fi
-    else
+    if [ "$kb" != "true" ]; then
       q_graphics="true"
       qdrive "-vga \"${q_intel_vga}\""
       qdrive "-display \"default\""
