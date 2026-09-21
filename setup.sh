232ccd7cbb258e41281d83832af85615ba9c26ae
jredfox <jredfox_@outlook.com>
2026-09-20 22:41:25 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -70,8 +70,10 @@ fi
 if ! command -v qemu-system-ppc64le >/dev/null 2>&1; then
     qemu_system_ppc64="$(command -v qemu-system-ppc64 2>/dev/null)"
     if [ ! -z "$qemu_system_ppc64" ]; then
-        echo "creating symlink qemu-system-ppc64le"
-        sudo ln -sfn "${name}.iso"
+        qppc64le="$(dirname "$qemu_system_ppc64")/qemu-system-ppc64le"
+        echo "creating symlink $qppc64le -> qemu-system-ppc64"
+        sudo ln -sfn "qemu-system-ppc64" "$qppc64le"
+        sudo chmod +x "$qppc64le"
     fi
 fi
 #make install_dir absolute
